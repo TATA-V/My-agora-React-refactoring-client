7d8d4373d7e30329bf95bed3b3d42a5c9663d95f
Choi-hyeona <xhrl523@naver.com>
2023-02-12 18:11:40 +0900

    아고라 스테이츠 리액트로 리팩토링 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Form } from "./components/Form";
+import { Discussions } from "./components/Discussions";
+import { TopLine } from "./components/TopLine";
 
 function App() {
+  const domain = "http://localhost:4000";
+  const [discussions, setDiscussions] = useState([]);
+
+  useEffect(() => {
+    getDiscussion();
+  }, []);
+
+  const getDiscussion = () => {
+    return fetch(domain + "/discussions")
+      .then((res) => res.json())
+      .then((json) => {
+        setDiscussions(json);
+      });
+  };
+
+  const addDiscussion = ({ author, title, bodyText }) => {
+    const newDiscussionData = { author, title, bodyText };
+    fetch(domain + "/discussions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDiscussionData),
+    }).then((res) => {
+      if (res.status === 201) {
+        getDiscussion();
+      }
+    });
+  };
+
+  const deleteDiscussion = (id) => {
+    fetch(domain + `/discussions/${id}`, {
+      method: "DELETE",
+    }).then((res) => {
+      if (res.status === 202 || res.status === 204) {
+        getDiscussion();
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopLine />
+      <Form addDiscussion={addDiscussion}></Form>
+      <Discussions discussions={discussions} deleteDiscussion={deleteDiscussion}></Discussions>
+    </>
   );
 }
 
